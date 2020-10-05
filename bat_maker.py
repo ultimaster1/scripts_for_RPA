44ab2034d89df80f6b5f72f32658b262efc6c6ed
ultimaster1 <k_tereschenko@mail.ru>
2020-10-05 15:01:22 +0300

    Update bat_maker.py

--- bat_maker.py
+++ bat_maker.py
@@ -11,6 +11,13 @@ bat_path = path_to_bat_files_store + 'action.bat'
 action_bat = open(bat_path,'w+')
 action_bat.write(command)
 action_bat.close()
+vbs_path = path_to_bat_files_store + 'command_vbs.vbs'
+vbs_file = open(vbs_path,'w+')
+vbs_file_command = "Set WshShell = CreateObject(\"WScript.Shell\")\n WshShell.Run chr(34) & \"" + bat_path +"\" & Chr(34), 0\nSet WshShell = Nothing"
+vbs_file.write(vbs_file_command)
+vbs_file.close()
+
+
 print('Do you want to create bat files to add your task in windows sheduler [y/n]?')
 y_n = input()
 if y_n == 'y':
@@ -20,7 +27,7 @@ if y_n == 'y':
     task_name = input()
     print('Type exact time in format HH:mm')
     exact_time = input()
-    create_command = 'schtasks /create /sc ' + shedule_type +' /tn ' + task_name + ' /tr ' +  bat_path + ' /st 11:25'
+    create_command = 'schtasks /create /sc ' + shedule_type +' /tn ' + task_name + ' /tr ' +  vbs_path + ' /st 11:25'
     delete_command = 'schtasks /delete /tn ' + task_name
     create_bat = open(path_to_bat_files_store + 'create.bat','w+')
     create_bat.write(create_command)
@@ -28,3 +35,5 @@ if y_n == 'y':
     delete_bat = open(path_to_bat_files_store + 'delete.bat','w+')
     delete_bat.write(delete_command)
     delete_bat.close()
+
+
